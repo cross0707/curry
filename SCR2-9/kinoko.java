e04d05c4cbd1b233caf1a740d444a84054abf2f6
a-a-a-a-a-a-a-a-a-a <14133@hakodate.kosen-ac.jp>
2018-11-22 11:19:18 +0900

    きのこ追加

---
+++ SCR2-9/kinoko.java
@@ -0,0 +1,19 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class kinoko here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class kinoko extends Actor
+{
+    /**
+     * Act - do whatever the kinoko wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
